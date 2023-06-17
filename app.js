c692f01cdc6e5072d6f4ed1e84b7b0845e2b0472
Luchioffice <uluann1@gmail.com>
2023-06-17 10:16:23 +0100

    teampage

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const picElement = document.querySelectorAll('.group_inside');
+picElement.forEach((el) => observer.observe(el));
